5f0b1257feecb374b05301128f0b25fa0f7ee5c1
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-27 10:27:54 +0000

    formatting code to be responsive

--- src/App.tsx
+++ src/App.tsx
@@ -9,38 +9,41 @@ type Photo = {
 function App() {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(false);
-  
+  const [color, setColor] = useState("bg-orange-500");
 
-  // useEffect(() => {
-  //   async function fetchPhotos() {
-  //     setLoading(true);
+  useEffect(() => {
+    async function fetchPhotos() {
+      setLoading(true);
 
-  //     const res = await fetch("https://api.github.com/users");
+      const res = await fetch("https://api.github.com/users");
 
-  //    const photos = await res.json();
+     const photos = await res.json();
 
-  //     setPhotos(photos);
-  //     setLoading(false);
-  //   }
-  //   fetchPhotos();
-  // }, []);
+      setPhotos(photos);
+      setLoading(false);
+    }
+    fetchPhotos();
+  }, []);
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-screen ">
+      <div className={`flex items-center justify-center h-screen`}>
         <div className="border-[16px] border-[ #f3f3f3] rounded-full w-[120px] h-[120px] border-t-[#3498db] animate-spin"></div>
       </div>
     );
   } else {
     return (
-      <div className="font-mono h-screen w-screen">
+      <div className={`font-mono min-h-screen ${color}`}>
         <div className="justify-center flex pt-10">
           <h1 className="text-3xl font-bold">Photo Gallery with Fetch</h1>
         </div>
 
         <div>
           <form>
-            <div className="flex justify-center mt-8">
+            <div className="flex flex-col items-center mt-8">
+              <div className="mb-8">
+                <h3 className="text-3xl">Select Theme</h3>
+              </div>
               <div className="flex w-[250px] justify-between">
                 <div>
                   <input
@@ -48,6 +51,8 @@ function App() {
                     name="theme"
                     id="orange"
                     className="peer hidden"
+                    value="bg-orange-500"
+                    onChange={() => setColor("bg-orange-500")}
                   />
                   <label
                     htmlFor="orange"
@@ -62,6 +67,7 @@ function App() {
                     name="theme"
                     id="blue"
                     className="peer hidden"
+                    onChange={() => setColor("bg-green-800")}
                   />
                   <label
                     htmlFor="blue"
@@ -76,6 +82,7 @@ function App() {
                     name="theme"
                     id="green"
                     className="peer hidden"
+                    onChange={() => setColor("bg-violet-800")}
                   />
                   <label
                     htmlFor="green"
@@ -90,10 +97,10 @@ function App() {
         </div>
 
         <div className=" justify-center flex mt-40">
-          <div className=" grid grid-cols-3 gap-4">
+          <div className=" md:grid md:grid-cols-3 md:gap-4">
             {photos.slice(0, 18).map((photo: Photo) => (
               <div
-                className="shadow-xl  w-80 flex hover:bg-blue-400"
+                className="shadow-xl  xl:w-80 flex hover:bg-blue-400 md:my-0 my-8 bg-white"
                 key={photo.id}
               >
                 <div className="w-1/2">
