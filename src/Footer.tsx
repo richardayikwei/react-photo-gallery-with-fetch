b5455d6792b8caf99ff5354b56c72367fd2bd8d3
Richard Ayikwei <richardayikweibee@gmail.com>
2025-05-22 10:55:10 +0000

    ref(remove): removing personal data in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,14 +3,14 @@
 const Footer = () => {
   return (
     <footer className=" flex text-[18px] flex-col items-center mt-9 h-[200px]">
-      <div className="flex md:justify-around items-center md:w-[700px] md:h-[150px] h-[100px] md:py-6 py-20 md:flex-row flex-col bg">
+      {/* <div className="flex md:justify-around items-center md:w-[700px] md:h-[150px] h-[100px] md:py-6 py-20 md:flex-row flex-col bg">
         <p className="text-[white] ">
           <em>Email</em>: richardayikweibee@gmail.com
         </p>
         <p className="text-[white] ">
           <em>Contact</em>: +233557500645
         </p>
-      </div>
+      </div> */}
 
       <div className="md:py-6 flex">
         <p className="text-[white]">
